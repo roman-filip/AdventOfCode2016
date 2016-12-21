25fae290c0259daa00d5aede09761adfa990fe51
roman-filip <dicky@email.cz>
2016-12-21 19:27:12 +0100

    Development of task for Day 2

--- AdventOfCode2016FSharp.Test/Day2Test.fs
+++ AdventOfCode2016FSharp.Test/Day2Test.fs
@@ -4,14 +4,51 @@ open Microsoft.VisualStudio.TestTools.UnitTesting
 open AdventOfCode2016FSharp.Day2
 
 [<TestClass>]
+type KeypadTest() =
+    [<TestMethod>]
+    member x.TestMoveLeft() =
+        Assert.AreEqual(4, (new Keypad()).GetNumber "L")
+
+    [<TestMethod>]
+    member x.TestMoveRight() =
+        Assert.AreEqual(6, (new Keypad()).GetNumber "R")
+
+    [<TestMethod>]
+    member x.TestMoveUp() =
+        Assert.AreEqual(2, (new Keypad()).GetNumber "U")
+
+    [<TestMethod>]
+    member x.TestMoveDown() =
+        Assert.AreEqual(8, (new Keypad()).GetNumber "D")
+
+    member x.TestMoveLeft3x() =
+        Assert.AreEqual(4, (new Keypad()).GetNumber "LLL")
+
+    [<TestMethod>]
+    member x.TestMoveRight3x() =
+        Assert.AreEqual(6, (new Keypad()).GetNumber "RRR")
+
+    [<TestMethod>]
+    member x.TestMoveUp3x() =
+        Assert.AreEqual(2, (new Keypad()).GetNumber "UUU")
+
+    [<TestMethod>]
+    member x.TestMoveDown3x() =
+        Assert.AreEqual(8, (new Keypad()).GetNumber "DDD")
+
+    [<TestMethod>]
+    member x.TestNumber1() =
+        Assert.AreEqual(1, (new Keypad()).GetNumber "ULL")
+
+
+[<TestClass>]
 type Day2Test() =
     let day2 = new Day2()
 
     [<TestMethod>]
-    [<Ignore>]
     member x.TestPart1Example1() =
         let result = day2.Part1 "TODO"
-        ()
+        Assert.Fail()
 
     [<TestMethod>]
     [<Ignore>]
---
+++ AdventOfCode2016FSharp/Day2/Keypad.fs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode2016FSharp.Day2
+
+type Keypad() = 
+    let mutable actualPossition = (1, 1)
+    let keypad =
+        [|
+            [| 1; 2; 3 |]
+            [| 4; 5; 6 |]
+            [| 7; 8; 9 |]
+        |]
+
+    let processInstruction instruction =
+        let actualX = fst actualPossition
+        let actualY = snd actualPossition
+        match instruction with
+        | 'L' -> if actualX > 0 then  actualPossition <- (actualX - 1, actualY)
+        | 'R' -> if actualX < keypad.[actualY].GetLength(0) - 1 then actualPossition <- (actualX + 1, actualY)
+        | 'U' -> if actualY > 0 then  actualPossition <- (actualX, actualY - 1)
+        | 'D' -> if actualY < keypad.GetLength(0) - 1 then actualPossition <- (actualX, actualY + 1)
+        | _ -> ()
+
+    let getNumberFromActualtPosition() =
+        keypad.[snd actualPossition].[fst actualPossition]
+
+    member this.GetNumber instructions =
+        instructions |> Seq.iter(fun instr -> processInstruction instr)
+        getNumberFromActualtPosition()
